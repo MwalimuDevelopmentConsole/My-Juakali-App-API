79f860343042385c4031b63cea963f3e99ff8019
peter514 <peterkgachora1@gmail.com>
2025-08-16 10:01:07 +0300

    updated allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,13 +1,10 @@
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://tweetyservices.org",
-  "https://www.tweetyservices.org",
-  "https://www.sheilaservice.sheilathewriter.com",
-  "https://sheilaservice.sheilathewriter.com",
-  "https://www.sheilathewriter.com",
   "http://192.168.100.59:5173",
   "http://192.168.100.55:5173",
+  "https://myjuakali.com",
+  "https://www.myjuakali.com",
 ];
 
 module.exports = allowedOrigins;
